0dd39a769c8aeac4720a00780f478399499a9e18
Vishnu S Reddy <vishnu.sr@shadowfax.in>
2022-07-30 00:06:04 +0530

    Requesting Permission for READ_PHONE_STATE in MainActivity

--- app/src/main/java/com/faanghut/detectsimchange/MainActivity.kt
+++ app/src/main/java/com/faanghut/detectsimchange/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.faanghut.detectsimchange
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermissionForReadPhoneState()
+    }
+
+    private fun requestPermissionForReadPhoneState() {
+        if (checkSelfPermission(android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(android.Manifest.permission.READ_PHONE_STATE), 1)
+        } else {
+            Toast.makeText(this, "Permission Already Granted", Toast.LENGTH_SHORT).show()
+        }
     }
 }
